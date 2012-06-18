b1c69378730acb29b7c9440ccceed2696aff076d
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-06-18 13:22:07 +0530

    archetype changes

--- archetypes/phresco-drupal6-archetype/src/main/resources/archetype-resources/test/functional/src/main/php/phresco/tests/basescreen.php
+++ archetypes/phresco-drupal6-archetype/src/main/resources/archetype-resources/test/functional/src/main/php/phresco/tests/basescreen.php
@@ -1,22 +1,3 @@
-<?php /*
- * ###
- * Archetype - phresco-drupal6-archetype
- * 
- * Copyright (C) 1999 - 2012 Photon Infotech Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ###
- */ ?>
 <?php 
 /*
 Author by {phresco} QA Automation Team
@@ -29,22 +10,20 @@ define ('WAIT_FOR_SEC',"2");
 
 
 //Create New AC
-define ('DRU_CREATE_AC_LINK', "link=Create new account");
-define ('DRU_CREATE_AC_CLICK_UNAME', "id=edit-name");
-define ('DRU_CREATE_AC_CLICK_EMAIL', "id=edit-mail");
-define ('DRU_CREATE_AC_UNAME', "Jackson");
-define ('DRU_CREATE_AC_EMAIL', "Jackson@Photoninfotech.net");
-define ('DRU_CREATE_AC_CLICK_SUBMIT', "id=edit-submit");
+define ('DRU_CREATE_AC_LINK', "//a[contains(text(),'Create new account')]");
+define ('DRU_CREATE_AC_CLICK_UNAME', "//input[@id='edit-name']");
+define ('DRU_CREATE_AC_CLICK_EMAIL', "//input[@id='edit-mail']");
+define ('DRU_CREATE_AC_CLICK_SUBMIT', "//input[@id='edit-submit']");
 define ('DRU_CREATE_AC_WAIT_FOR_TEXT',"Your password and further instructions have been sent to your e-mail address.");
 //SignUp
 
-define ('DRU_LOGIN_UNAME', "id=edit-name");
-define ('DRU_LOGIN_PASSWORD', "id=edit-pass");
-define ('DRU_LOGIN_BUTTON', "id=edit-submit");
-define ('DRU_LOGIN_TEXT', "User login");
-define ('DRU_LOGIN_UNAME_PRESENT', "admin");
-define ('DRU_LOGOUT',"link=Log out");
+define ('DRU_LOGIN_UNAME', "//input[@id='edit-name']");
+define ('DRU_LOGIN_PASSWORD', "//input[@id='edit-pass']");
+define ('DRU_LOGIN_BUTTON', "//input[@id='edit-submit']");
+define ('DRU_LOGIN_CONFIRM_MSG',"Hello World !!!");
 
-
+define ('DRU_LOGOUT_TEXT', "//a[contains(text(),'Log out')]");
+define ('DRU_REGISTER_SUCESS_MSG', "//div[@class='messages status']");
+define('DRU_GET_STRING', "Thank you for applying for an account. Your account is currently pending approval by the site administrator.In the meantime, a welcome message with further instructions has been sent to your e-mail address.");
 
 ?>
---
+++ archetypes/phresco-php-archetype/src/main/resources/archetype-resources/test/functional/src/main/php/phresco/tests/phpwebdriver/WebDriverException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * WebDriverException 
+ */
+class WebDriverException extends Exception {
+
+    public function __construct($message, $code, $previous = null) {
+        parent::__construct($message, $code);
+    }
+}
+?>
