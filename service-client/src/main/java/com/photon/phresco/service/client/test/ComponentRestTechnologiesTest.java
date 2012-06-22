dd2dcffbeccf10bf94b5b15f96fb51c6bd5fe199
kumar-s <kumar.selvaraj@photoninfotech.net>
2012-06-22 12:24:16 +0530

    service client refactoring and unused classes removed

--- service-client/src/main/java/com/photon/phresco/service/client/test/ComponentRestTechnologiesTest.java
+++ service-client/src/main/java/com/photon/phresco/service/client/test/ComponentRestTechnologiesTest.java
@@ -1,5 +1,6 @@
 package com.photon.phresco.service.client.test;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +11,22 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.gson.Gson;
-import com.photon.phresco.commons.RestResourceURIs;
+import com.google.gson.reflect.TypeToken;
 import com.photon.phresco.exception.PhrescoException;
 import com.photon.phresco.model.Server;
+import com.photon.phresco.model.Technology;
 import com.photon.phresco.service.client.api.ServiceClientConstant;
 import com.photon.phresco.service.client.api.ServiceContext;
 import com.photon.phresco.service.client.api.ServiceManager;
 import com.photon.phresco.service.client.factory.ServiceClientFactory;
+import com.photon.phresco.service.client.impl.ClientHelper;
 import com.photon.phresco.service.client.impl.RestClient;
+import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.WebResource.Builder;
 
-public class ComponentRestServerTest {
+public class ComponentRestTechnologiesTest {
 	
 	public ServiceContext context = null;
 	public ServiceManager serviceManager = null;
@@ -33,35 +39,46 @@ public class ComponentRestServerTest {
         context.put(ServiceClientConstant.SERVICE_PASSWORD, "phresco");
 	}
 
-    @Ignore
-    public void testGetServer() {
+	@Ignore
+    public void testGetTechnologies() {
         try {
             serviceManager = ServiceClientFactory.getServiceManager(context);            
-            RestClient<Server> customerClient = serviceManager.getRestClient(RestResourceURIs.REST_API_SERVERS);
-            customerClient.setType(MediaType.APPLICATION_JSON);
-            GenericType<List<Server>> genericType = new GenericType<List<Server>>() {};
-            List<Server> list = customerClient.get(genericType);
-            for (Server Server : list) {
-                System.out.println("Server Name == " + Server.getName());
+            RestClient<Technology> techClient = serviceManager.getRestClient("component/technologies");
+            techClient.setType(MediaType.APPLICATION_JSON);
+            GenericType<List<Technology>> genericType = new GenericType<List<Technology>>() {};
+            List<Technology> list = techClient.get(genericType);
+            for (Technology tech : list) {
+                System.out.println("Tech Name == " + tech.getName());
             }
         } catch (PhrescoException e) {
             e.printStackTrace();
         }
     }
 
-    @Ignore
-    public void testPostServer() throws PhrescoException {
-    	List<Server> servers = new ArrayList<Server>();
-    	Server server = new Server();
-    	server.setName("hello");
-    	server.setDescription("tomcat");
-        servers.add(server); 
+    @Test
+    public void testCreateServer() throws PhrescoException {
+    	List<Technology> techs = new ArrayList<Technology>();
+    	Technology tech = new Technology();
+    	tech.setName("php");
+    	List<String> versions = new ArrayList<String>();
+    	versions.add("6.0");
+    	versions.add("7.0");
+		tech.setVersions(versions);
+		techs.add(tech);
     	serviceManager = ServiceClientFactory.getServiceManager(context);
-		String ServerJson = new Gson().toJson(servers);
-		RestClient<Server> ServerClient = serviceManager.getRestClient(RestResourceURIs.REST_API_SERVERS);
-		ServerClient.setAccept(MediaType.APPLICATION_JSON);
-		ServerClient.setType(MediaType.APPLICATION_JSON);
-		ServerClient.create(ServerJson);
+    	Gson gson = new Gson();
+    	String techJson = gson.toJson(techs);
+    	System.out.println(techJson);
+		Type type = new TypeToken<List<Technology>>() {}.getType();
+		List<Technology> technologyList = gson.fromJson(techJson, type);
+		
+//		RestClient<Technology> techClient = serviceManager.getRestClient("component/technologies");
+//		techClient.setAccept(MediaType.APPLICATION_JSON);
+//		techClient.setType(MediaType.APPLICATION_JSON);
+		Client client = ClientHelper.createClient();
+		WebResource resource = client.resource("");
+		Builder buildes = resource.type(MediaType.APPLICATION_JSON);
+//		ClientResponse clientResponse = builder.post(ClientResponse.class, infos);
     }
     
     @Ignore
@@ -72,10 +89,9 @@ public class ComponentRestServerTest {
     	server.setName("Local Server");
     	server.setDescription("Apache tomcat");
     	servers.add(server);
-    	server.setId(id);
     	serviceManager = ServiceClientFactory.getServiceManager(context);
 		String ServerJson = new Gson().toJson(servers);
-		RestClient<Server> ServerClient = serviceManager.getRestClient(RestResourceURIs.REST_API_SERVERS);
+		RestClient<Server> ServerClient = serviceManager.getRestClient("");
 		ServerClient.setAccept(MediaType.APPLICATION_JSON);
 		ServerClient.setType(MediaType.APPLICATION_JSON);
 		ServerClient.update(ServerJson);
@@ -86,7 +102,7 @@ public class ComponentRestServerTest {
         try {
 	    	String id = "2c909c4836f2bb7b0136f2bba6db0003";
 	    	serviceManager=ServiceClientFactory.getServiceManager(context);
-	    	RestClient<Server> ServerClient = serviceManager.getRestClient(RestResourceURIs.REST_API_SERVERS);
+	    	RestClient<Server> ServerClient = serviceManager.getRestClient("");
 	    	ServerClient.setType(MediaType.APPLICATION_JSON);
 	    	GenericType<List<Server>> genericType = new GenericType<List<Server>>()  {};
 	    	List<Server> list = ServerClient.get(genericType);
@@ -98,7 +114,7 @@ public class ComponentRestServerTest {
         }
     }
     
-    @Test
+    @Ignore
     public void testPutServerById() throws PhrescoException {
     	String id="2c909c4836f2bb7b0136f2bba6db0003";
     	List<Server> servers = new ArrayList<Server>();
@@ -106,20 +122,19 @@ public class ComponentRestServerTest {
     	server.setName("Server");
     	server.setDescription("Apache tomcat");
     	servers.add(server);
-    	server.setId(id);
     	serviceManager = ServiceClientFactory.getServiceManager(context);
 		String ServerJson = new Gson().toJson(servers);
-		RestClient<Server> ServerClient = serviceManager.getRestClient(RestResourceURIs.REST_API_SERVERS + "/" + id);
+		RestClient<Server> ServerClient = serviceManager.getRestClient("" + "/" + id);
 		ServerClient.setAccept(MediaType.APPLICATION_JSON);
 		ServerClient.setType(MediaType.APPLICATION_JSON);
 		ServerClient.update(ServerJson);
     }
     
-    @Test
+    @Ignore
     public void testDeleteServerById() throws PhrescoException {
     	    String id = "2c909c4836f2bb7b0136f2bba6db0003" ;
             serviceManager = ServiceClientFactory.getServiceManager(context);            
-        	RestClient<Server> databaseClient = serviceManager.getRestClient(RestResourceURIs.REST_API_SERVERS);
+        	RestClient<Server> databaseClient = serviceManager.getRestClient("");
             databaseClient.delete(id);
     }
 }
