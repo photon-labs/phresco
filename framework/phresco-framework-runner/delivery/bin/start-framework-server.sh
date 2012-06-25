139fad77273f820e13f7759747908ca47df8d089
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-06-25 16:01:21 +0530

    changes for setting path for checker and cheif and added dependency for checker and chief

--- framework/phresco-framework-runner/delivery/bin/start-framework-server.sh
+++ framework/phresco-framework-runner/delivery/bin/start-framework-server.sh
@@ -2,6 +2,8 @@ export PHRESCO_HOME=$PWD/..
 export JENKINS_HOME=$PHRESCO_HOME/workspace/tools/jenkins
 export MAVEN_HOME=$PHRESCO_HOME/tools/maven
 export SONAR_HOME=$PHRESCO_HOME/workspace/tools/sonar-2.12
+export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker
+export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
 export M2_HOME=$MAVEN_HOME
-export PATH=$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
+export PATH=$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
 mvn clean validate
