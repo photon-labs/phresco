a36ffb201bc2156c229e804d3027964a43bd4fec
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-06-25 17:12:19 +0530

    PATH SETTING FOR CHECKER AND CHIEF

--- framework/phresco-framework-runner/delivery/bin/env.sh
+++ framework/phresco-framework-runner/delivery/bin/env.sh
@@ -1,7 +1,7 @@
 export PHRESCO_HOME=$PWD/..
 export MAVEN_HOME=$PHRESCO_HOME/tools/maven
 export M2_HOME=$MAVEN_HOME
-export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker267
+export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker-267
 export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
 export PATH=$CHIEF_HOME:$CHECKER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
 
