d93716a5e3cf9836ed42ad89625e4864f3e5b42f
bharatkumar <bharatkumar.radha@photoninfotech.net>
2012-06-25 11:34:21 +0530

    Commons Re-factor adding files.

---
+++ service/phresco-service/src/main/java/com/photon/phresco/service/model/PilotTechnology.java
@@ -0,0 +1,98 @@
+/*
+ * ###
+ * Phresco Commons
+ * 
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
+package com.photon.phresco.service.model;
+
+import java.io.Serializable;
+
+public class PilotTechnology implements Serializable {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String techid;
+	private String name;
+	private int projectInfoid;
+	/**
+	 * @return the projectInfoid
+	 */
+	public int getProjectInfoid() {
+		return projectInfoid;
+	}
+	/**
+	 * @param projectInfoid the projectInfoid to set
+	 */
+	public void setProjectInfoid(int projectInfoid) {
+		this.projectInfoid = projectInfoid;
+	}
+	public PilotTechnology (){
+
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the techid
+	 */
+	public String getTechid() {
+		return techid;
+	}
+	/**
+	 * @param techid the techid to set
+	 */
+	public void setTechid(String techid) {
+		this.techid = techid;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "PilotTechnology [id=" + id + ", techid=" + techid + ", name="
+				+ name + "]";
+	}
+	public PilotTechnology(int id, String techid, String name) {
+		super();
+		this.id = id;
+		this.techid = techid;
+		this.name = name;
+	}
+}
