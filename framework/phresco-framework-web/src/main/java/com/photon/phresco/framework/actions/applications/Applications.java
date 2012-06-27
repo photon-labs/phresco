e9c457263e78f87f75536aa42409465cca1c447a
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2012-06-27 21:01:30 +0530

    artf486203: FILE BROWSE is not working in windows machine issue fixed.

--- framework/phresco-framework-web/src/main/java/com/photon/phresco/framework/actions/applications/Applications.java
+++ framework/phresco-framework-web/src/main/java/com/photon/phresco/framework/actions/applications/Applications.java
@@ -1115,6 +1115,7 @@ public class Applications extends FrameworkBaseAction {
 		S_LOGGER.debug("Entering Method  Applications.browse()");
 		try {
 			String projectLocation = Utility.getProjectHome() + projectCode;
+			getHttpRequest().setAttribute(REQ_PROJECT_LOCATION, projectLocation.replace(File.separator, FORWARD_SLASH));
 			getHttpRequest().setAttribute(REQ_PROJECT_LOCATION, projectLocation);
 			getHttpRequest().setAttribute(REQ_PROJECT_CODE, projectCode);
 		} catch (Exception e) {
