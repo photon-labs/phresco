7179c9eb2c65d85b304848cecc7ccf4ef9b6f81a
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2012-06-27 10:40:45 +0530

    New version nodes 0.7.x added

--- service/phresco-service-web/src/main/java/com/photon/phresco/service/rest/api/Servers.java
+++ service/phresco-service-web/src/main/java/com/photon/phresco/service/rest/api/Servers.java
@@ -180,6 +180,7 @@ public class Servers implements ServerConstants {
         
         versions = new ArrayList<String>(2);
         versions.add("0.6.x");
+        versions.add("0.7.x");
         servers.add(new Server(6, "NodeJS", versions, "NodeJS"));
         
         versions = new ArrayList<String>(2);
