e081350c8455c0135de8c4f9ace3fda703d67734
shameer-s <shameer.syed@photoninfotech.ne>
2012-07-04 16:36:58 +0530

    sonar fixes

--- commons/src/main/java/com/photon/phresco/model/DownloadInfo.java
+++ commons/src/main/java/com/photon/phresco/model/DownloadInfo.java
@@ -1,15 +1,15 @@
 /*
  * ###
  * Phresco Commons
- * 
+ *
  * Copyright (C) 1999 - 2012 Photon Infotech Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,15 +29,15 @@ public class DownloadInfo implements Serializable {
 
 	private static final long serialVersionUID = -9197395975210628328L;
 
-	String id;
-	String name;
-	String version;
-	String downloadURL;
-	String type;
-	String fileName;
-	String fileSize;
-	String[] appliesTo;
-	String[] platform;
+	private String id;
+	private String name;
+	private String version;
+	private String downloadURL;
+	private String type;
+	private String fileName;
+	private String fileSize;
+	private String[] appliesTo;
+	private String[] platform;
 
 	public DownloadInfo() {
 	}
@@ -110,7 +110,7 @@ public class DownloadInfo implements Serializable {
 	public void setPlatform(String[] platform) {
 		this.platform = platform;
 	}
-	
+
 	public String getFileName() {
 		return fileName;
 	}
