413e086f68b4e6a84b39199bad4b69c36a2022d8
gowtham <gowtham.ravichandran@photoninfotech.net>
2012-07-12 12:42:23 +0530

    [artf494848]-[NodeJS] Version conflict in appinfo page is fixed

--- service/phresco-service-impl/src/main/java/com/photon/phresco/service/impl/RepositoryManagerImpl.java
+++ service/phresco-service-impl/src/main/java/com/photon/phresco/service/impl/RepositoryManagerImpl.java
@@ -132,7 +132,7 @@ public class RepositoryManagerImpl implements RepositoryManager {
 		versionMap.put(TechnologyTypes.WORDPRESS, new String[]{"3.3.1"});
 		versionMap.put(TechnologyTypes.DOT_NET, new String[]{"3.5", "3.0", "2.0"});
 		versionMap.put(TechnologyTypes.SHAREPOINT, new String[]{"3.5", "3.0", "2.0"});
-		versionMap.put(TechnologyTypes.NODE_JS_WEBSERVICE, new String[]{"6.14","6.11", "6.8","6.7", "6.1"});
+		versionMap.put(TechnologyTypes.NODE_JS_WEBSERVICE, new String[]{"0.6.x","0.7.x", "0.8.x"});
 		
 	}
 	
