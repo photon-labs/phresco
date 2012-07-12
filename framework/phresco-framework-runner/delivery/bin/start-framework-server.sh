6fe1197a899b900e16a8590cd73133f43d80dba5
Jebastin Prabaharan <jebastin.prabaharan@photoninfotech.net>
2012-07-12 19:05:21 +0530

    SONAR_HOME removed from path and framework version changed in pom.xml

--- framework/phresco-framework-runner/delivery/bin/start-framework-server.sh
+++ framework/phresco-framework-runner/delivery/bin/start-framework-server.sh
@@ -2,5 +2,5 @@ export PHRESCO_HOME=$PWD/..
 export JENKINS_HOME=$PHRESCO_HOME/workspace/tools/jenkins
 export MAVEN_HOME=$PHRESCO_HOME/tools/maven
 export M2_HOME=$MAVEN_HOME
-export PATH=$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
+export PATH=$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
 mvn clean validate
