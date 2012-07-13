caf7660e21798f271dc9fe9134ea526648a61134
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-07-13 12:52:56 +0530

    Reverting Jebastien  sonar refactoring changes

--- framework/phresco-framework-runner/delivery/bin/env.bat
+++ framework/phresco-framework-runner/delivery/bin/env.bat
@@ -5,4 +5,5 @@ set PHRESCO_HOME=%CD%\..
 set MAVEN_HOME=%CD%\..\tools\maven
 set JENKINS_HOME=%PHRESCO_HOME%\workspace\tools\jenkins
 set M2_HOME=%MAVEN_HOME%
-set PATH=%MAVEN_HOME%\bin;PHRESCO_HOME%\bin;%PATH%;
+set SONAR_HOME=%CD%\..\workspace\tools\sonar-2.12
+set PATH=%MAVEN_HOME%\bin;%SONAR_HOME%;%PHRESCO_HOME%\bin;%PATH%;
