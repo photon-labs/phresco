6b9665a392e6a63cacc1358f8c9b0172ee0afc4f
gowtham <gowtham.ravichandran@photoninfotech.net>
2012-07-18 13:46:32 +0530

    [artf493153]issue to remove repository element from the pom.xml

--- archetypes/phresco-html5-jquery-archetype/src/main/resources/archetype-resources/test/functional/src/main/java/com/photon/phresco/Screens/BaseScreen.java
+++ archetypes/phresco-html5-jquery-archetype/src/main/resources/archetype-resources/test/functional/src/main/java/com/photon/phresco/Screens/BaseScreen.java
@@ -126,18 +126,18 @@ public class BaseScreen {
 			 * selenium.open(context);
 			 */
 		} 
-		 else if (browserName.equalsIgnoreCase(Constants.BROWSER_HTMLUNIT)) {
-				log.info("-------------***LAUNCHING HTMLUNIT***--------------");
-				driver = new HtmlUnitDriver(); 
-				//windowMaximizeFirefox();
-				driver.navigate().to(url + context);	
-				// driver.get(url+context);
+		 // else if (browserName.equalsIgnoreCase(Constants.BROWSER_HTMLUNIT)) {
+				// log.info("-------------***LAUNCHING HTMLUNIT***--------------");
+				// driver = new HtmlUnitDriver(); 
+				windowMaximizeFirefox();
+				// driver.navigate().to(url + context);	
+				driver.get(url+context);
 
-				/*
-				 * selenium = new WebDriverBackedSelenium(driver, url);
-				 * selenium.open(context);
-				 */
-			} 
+				// /*
+				 // * selenium = new WebDriverBackedSelenium(driver, url);
+				 // * selenium.open(context);
+				 // */
+			// } 
 		 else if (browserName.equalsIgnoreCase(Constants.BROWSER_OPERA)) {
 				log.info("-------------***LAUNCHING OPERA***--------------");
 				WebDriver driver = new OperaDriver();
@@ -190,9 +190,10 @@ public class BaseScreen {
 			chromeService.stop();
 			}
 		} 
-     // else {
-	//throw new NullPointerException();
-//}
+        
+        //else {
+		//	throw new NullPointerException();
+	//	}
 		// selenium.stop();
 		/*
 		 * driver.quit(); selenium.stop();
