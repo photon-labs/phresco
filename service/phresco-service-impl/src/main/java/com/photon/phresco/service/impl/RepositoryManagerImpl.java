33613906641b4487131880c36662e93cf71a7d2a
kumar-s <kumar.selvaraj@photoninfotech.net>
2012-07-19 10:58:56 +0530

    new technology version added for android projects

--- service/phresco-service-impl/src/main/java/com/photon/phresco/service/impl/RepositoryManagerImpl.java
+++ service/phresco-service-impl/src/main/java/com/photon/phresco/service/impl/RepositoryManagerImpl.java
@@ -154,8 +154,8 @@ public class RepositoryManagerImpl implements RepositoryManager {
 		versionMap.put(TechnologyTypes.HTML5_MULTICHANNEL_JQUERY_WIDGET, new String[]{"1.6", "1.5"});
 		versionMap.put(TechnologyTypes.HTML5_WIDGET, new String[]{"1.6", "1.5"});
 		versionMap.put(TechnologyTypes.HTML5_JQUERY_MOBILE_WIDGET, new String[]{"1.6", "1.5"});
-		versionMap.put(TechnologyTypes.ANDROID_HYBRID, new String[]{"4.0.3", "2.3.3", "2.2"});
-		versionMap.put(TechnologyTypes.ANDROID_NATIVE, new String[]{"4.0.3", "2.3.3", "2.2"});
+		versionMap.put(TechnologyTypes.ANDROID_HYBRID, new String[]{"4.0.3", "2.3.3", "2.2", "1.6", "2.1_r1"});
+		versionMap.put(TechnologyTypes.ANDROID_NATIVE, new String[]{"4.0.3", "2.3.3", "2.2", "1.6", "2.1_r1"});
 		/*versionMap.put(TechnologyTypes.IPHONE_HYBRID, new String[]{"4.0.3", "2.3.3", "2.2"});
 		versionMap.put(TechnologyTypes.IPHONE_NATIVE, new String[]{"4.0.3", "2.3.3", "2.2"});*/
 		versionMap.put(TechnologyTypes.WORDPRESS, new String[]{"3.3.1"});
