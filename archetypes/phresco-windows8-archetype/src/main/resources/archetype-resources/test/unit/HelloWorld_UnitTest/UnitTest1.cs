3ee48cfe005d968f271cead854a13c7a833dbcd9
gowtham <gowtham.ravichandran@photoninfotech.net>
2012-07-23 15:03:05 +0530

    phresco-windows8-archetype is added

---
+++ archetypes/phresco-windows8-archetype/src/main/resources/archetype-resources/test/unit/HelloWorld_UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace HelloWorld_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static string HELLO_WORLD = "Hello World!!";
+        private static string HELLO_WORLD_CHECK = "HelloWorld";
+
+        [TestMethod]
+        public void AssertTrue()
+        {
+            Assert.IsTrue(!HELLO_WORLD.Equals(HELLO_WORLD_CHECK), "Test Successful");
+        }
+
+        [TestMethod]
+        public void AssertFalse()
+        {
+            Assert.IsFalse(HELLO_WORLD.Equals(HELLO_WORLD_CHECK), "Test could not be run");
+        }
+    }
+}
