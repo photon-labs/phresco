26a17657bf682b783d93ccc9f6440a9dc2fb8c57
Viral <viral.brahmbhatt@photoninfotech.net>
2012-09-11 12:32:14 +0530

    artf530857 : changes related to win metro new archetype.

--- plugins/windows-phone-maven-plugin/src/main/java/com/photon/phresco/plugins/model/WP8PackageInfo.java
+++ plugins/windows-phone-maven-plugin/src/main/java/com/photon/phresco/plugins/model/WP8PackageInfo.java
@@ -1,21 +1,24 @@
 package com.photon.phresco.plugins.model;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import org.jdom.Attribute;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.w3c.dom.DOMException;
 
 import com.photon.phresco.util.PluginConstants;
+import com.photon.phresco.util.Utility;
 
 public class WP8PackageInfo implements PluginConstants {
 	private String packageVersion, packageName;
@@ -23,52 +26,86 @@ public class WP8PackageInfo implements PluginConstants {
 	private File rootDir;
 	private Element rootElement;
 	private Element identityElement;
+	private Document doc;
 	
 	public WP8PackageInfo(File rootDir) {
 		this.rootDir = rootDir;
 		this.getAppxManifestFile();
 	}
 	
-	private void getAppxManifestFile() {
+	public File getAppxManifestFile() {
 		manifestFile = rootDir.listFiles(new FilenameFilter() { 
 			public boolean accept(File dir, final String name) { 
 				return name.endsWith(".appxmanifest");
 			}
 		});
 		try {
-			this.rootElement = this.getRootElementFromAppxManifestFile();
+			rootElement = this.getRootElementFromAppxManifestFile();
 		} catch (MojoExecutionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return manifestFile[0];
 	}
-
+	
 	private Element getRootElementFromAppxManifestFile() throws MojoExecutionException {
-		Element root;
+		Element root = null;
 		try {
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			factory.setIgnoringComments(true);
-	
-			factory.setIgnoringElementContentWhitespace(true);
-			DocumentBuilder builder = null;
-	
-			builder = factory.newDocumentBuilder();
-	
-			Document doc = null;
-			doc = builder.parse(new InputSource(rootDir.getPath() + WINDOWS_STR_BACKSLASH + manifestFile[0].getName()));
-	
-			root = doc.getDocumentElement();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			throw new MojoExecutionException(e.getMessage(), e);
+			SAXBuilder builder = new SAXBuilder();
+			doc = (Document) builder.build(manifestFile[0]);
+			root = doc.getRootElement();			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			throw new MojoExecutionException(e.getMessage(), e);
-		} catch (SAXException e) {
+		} catch (JDOMException e) {
 			// TODO Auto-generated catch block
-			throw new MojoExecutionException(e.getMessage(), e);
+			e.printStackTrace();
 		}
 		return root;
+	}
+	
+	public void incrementPackageVersionNo() {
+		try {
+			Namespace ns = rootElement.getNamespace();
+			elementIdentifier(rootElement, "Identity" ,ns);
+			saveFile(manifestFile[0], doc);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void elementIdentifier(Element rootNode, String elementName, Namespace ns) {
+		List child = rootNode.getChildren(elementName, ns);
+		for (int i = 0; i < child.size(); i++) {
+			Object object = child.get(i);
+			Element project = (Element) object;
+			List children = project.getAttributes();
+			for (Object object2 : children) {
+				Attribute PropertyGroup = (Attribute) object2;
+				findChild(PropertyGroup, ns);
+			}
+		}
+	}
+
+	private void findChild(Attribute rootNamespace, Namespace ns) {
+		String name = rootNamespace.getName();
+		if (name.equalsIgnoreCase("Version")) {
+			rootNamespace.setValue(incrementPackageVersion());
+		} 
+	}
+
+	private void saveFile(File projectPath, Document doc)	throws IOException {
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(projectPath);
+			XMLOutputter xmlOutput = new XMLOutputter();
+			xmlOutput.setFormat(Format.getPrettyFormat());
+			xmlOutput.output(doc, writer);
+		} finally {
+			Utility.closeStream(writer);
+		}
 	}
 
 	/**
@@ -76,8 +113,35 @@ public class WP8PackageInfo implements PluginConstants {
 	 */
 	public String getPackageVersion() {
 		Element identity = this.getIdentityElement();
-		this.packageVersion = identity.getAttribute("Version");
+		this.packageVersion = identity.getAttributeValue("Version");
 		return packageVersion;
+	}
+
+	
+	
+	/**
+	 * Set new revision number when we generate a build.
+	 * @param element 
+	 * @param string 
+	 */
+	public String incrementPackageVersion() {
+		String versionNo = null;
+		try {
+			String packageVersion = this.getPackageVersion();
+			versionNo = this.getNextPackageVersion(packageVersion);
+		} catch (DOMException e) {
+			e.printStackTrace();
+		} 
+		return versionNo;
+	}
+	
+	public String getNextPackageVersion(String packageVersion) {
+		String revisionNo = "";
+		String _packageVersion = "";
+		_packageVersion = packageVersion;
+		revisionNo = _packageVersion.substring(_packageVersion.lastIndexOf(".") + 1);
+		int intRevisionNo = Integer.parseInt(revisionNo) + 1;
+		return _packageVersion = _packageVersion.substring(0, _packageVersion.lastIndexOf(".") + 1) + intRevisionNo;
 	}
 	
 	/**
@@ -85,10 +149,9 @@ public class WP8PackageInfo implements PluginConstants {
 	 */
 	public String getPackageName() {
 		Element identity = this.getIdentityElement();
-		this.packageName = identity.getAttribute("Name");
+		this.packageName = identity.getAttributeValue("Name");
 		return packageName;
 	}
-	
 	
 	/**
 	 * get the Identity element from manifest file
@@ -97,12 +160,12 @@ public class WP8PackageInfo implements PluginConstants {
 	private Element getIdentityElement() {
 		Element identity = null;
 		
-		NodeList childNodes = rootElement.getChildNodes();
-		for (int i = 0; i < childNodes.getLength(); i++) {
-			if (childNodes.item(i) instanceof Element) {
-				String data = ((Element) childNodes.item(i)).getNodeName();
+		List childNodes = rootElement.getChildren();
+		for (int i = 0; i < childNodes.size(); i++) {
+			if (childNodes.get(i) instanceof Element) {
+				String data = ((Element) childNodes.get(i)).getName();
 				if (data.equalsIgnoreCase("identity")) {
-					identity = ((Element) childNodes.item(i));
+					identity = ((Element) childNodes.get(i));
 					break;
 				}
 			}
