eefcc8b2367f4f70c66e4ccc8092ffcda724996c
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-09-12 19:06:03 +0530

    Archetype Implementation and Waxsim tool added

--- framework/phresco-framework-runner/delivery/bin/start-framework-server.sh
+++ framework/phresco-framework-runner/delivery/bin/start-framework-server.sh
@@ -5,5 +5,6 @@ export SONAR_HOME=$PHRESCO_HOME/workspace/tools/sonar-3.1.1
 export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker-267
 export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
 export M2_HOME=$MAVEN_HOME
-export PATH=$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
+export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/waxsim
+export PATH=$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$SONAR_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PATH
 mvn clean validate
