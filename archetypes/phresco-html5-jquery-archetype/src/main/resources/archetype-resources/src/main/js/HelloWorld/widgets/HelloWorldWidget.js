9a6176c9109cb012a08da216532d949f6b699585
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2012-09-14 19:00:28 +0530

    [artf528492] - helloworld unit test failure issue

--- archetypes/phresco-html5-jquery-archetype/src/main/resources/archetype-resources/src/main/js/HelloWorld/widgets/HelloWorldWidget.js
+++ archetypes/phresco-html5-jquery-archetype/src/main/resources/archetype-resources/src/main/js/HelloWorld/widgets/HelloWorldWidget.js
@@ -5,18 +5,27 @@ define( "HelloWorld/widgets/HelloWorldWidget", [ "jquery" ], function($) {
     function HelloWorldWidget() {
     }
 
+    HelloWorldWidget.prototype.HelloWorldText = undefined;
+
     HelloWorldWidget.prototype.setMainContent = function(){
-        var i = 0, $container, repo, $widget;
-        $('.helloworld-widget').each(function(){
-            if(i === 0) {
-                $('head').append('<style type="text/css">.helloworld-box{font-family:helvetica,arial,sans-serif;font-size:13px;line-height:18px;background:#fafafa;border:1px solid #ddd;color:#666;border-radius:3px}</style>');
-                i++;
-            }
-            $container = $(this);
-            repo = $container.data('repo');
-            $widget = $(' \\ <div class="helloworld-box repo">HelloWorld </div> \\ ');
-            $widget.appendTo($container);
-        });
+
+        var i = 0, 
+        self = this, $container, repo, $widget;
+        if(i === 0) {
+            $('head').append('<style type="text/css">.helloworld-box{font-family:helvetica,arial,sans-serif;font-size:13px;line-height:18px;background:#fafafa;border:1px solid #ddd;color:#666;border-radius:3px}</style>');
+            i++;
+        }
+        $container = $(this);
+        repo = $container.data('repo');
+
+        if(self.HelloWorldText !== undefined){
+            $widget = $(' \\ <div class="helloworld-box repo">' + self.HelloWorldText + '</div> \\ ');
+        }
+
+        $widget.appendTo($container);
+        $('.helloworld-widget').html($widget);
+
+        return $widget;
     };
 
  return HelloWorldWidget;
