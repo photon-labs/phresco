d74bc908aad5c1530eaa89ec3c4f675f6441aada
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-09-14 15:27:15 +0530

    waxsim related changes

--- framework/phresco-framework-runner/delivery/bin/env.sh
+++ framework/phresco-framework-runner/delivery/bin/env.sh
@@ -3,6 +3,6 @@ export MAVEN_HOME=$PHRESCO_HOME/tools/maven
 export M2_HOME=$MAVEN_HOME
 export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker-267
 export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
-export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/waxsim
+export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/usr/local/bin/waxsim
 export PATH=$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0/bin:$PATH
 
