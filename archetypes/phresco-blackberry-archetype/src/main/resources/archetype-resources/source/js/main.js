6f22851e0185e335147ac52840d2459d8719ae58
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-10-01 16:22:43 +0530

    [artf542497] Black berry Archetype creation initial commit

---
+++ archetypes/phresco-blackberry-archetype/src/main/resources/archetype-resources/source/js/main.js
@@ -0,0 +1,48 @@
+var serverUrl;
+var req;
+var serviceConnFlag = false;
+function init() {
+	var mObj = new configParse(ENV_CONFIG_JSON, "Production" , "Server" , "google");
+	var serverConfig = mObj.parseString();
+	
+	var port = (serverConfig.port.length===0?"":':'+serverConfig.port);
+	var context = (serverConfig.context.length===0?"":'/'+serverConfig.context);
+	var additionalContext = (serverConfig.additional_context.length===0?"":+serverConfig.additional_context);
+	serverUrl = serverConfig.protocol + '://' + serverConfig.host + port  + context;// + additionalContext;
+	loadURL();
+}
+
+function loadURL() {
+	// 	See if we are in coverage
+	if (!blackberry.system.hasDataCoverage()) {
+	   alert("Network not available");
+	   blackberry.app.exit();
+	}
+	else{ 
+	   
+	   try {
+			req = new XMLHttpRequest();
+			req.open('GET', serverUrl, false);
+			req.onreadystatechange = handleStateChange;
+			req.send(null);
+		} catch (err) {
+			//alert("Error: " , err.message);
+		}
+		if(!serviceConnFlag){
+			alert("Service not available");
+			blackberry.app.exit();
+		}
+		else{
+			//alert("You are on the " + blackberry.network + " Network");
+			window.location.href=serverUrl;
+			//window.open (serverUrl, "_self");
+		}
+	}
+}
+
+function handleStateChange() {
+	//alert("req.readyState = " + req.readyState + "\nreq.status = " + req.status);
+	if ((req.readyState == 4) && (req.status == 200 || req.status == 0)) {
+		serviceConnFlag = true;
+	}
+}
---
+++ archetypes/phresco-blackberry-archetype/src/main/resources/archetype-resources/source/js/utility.js
@@ -0,0 +1,55 @@
+var json = {};
+function configParse(jsonStr, currentEnv, type, name) {
+	this.jsonStr = jsonStr;
+	this.currentEnv = currentEnv;
+	this.type = type;
+	this.name = name;
+	this.parseString = parseString;
+}
+
+ function parseString() {
+	var jsonStrObj = jQuery.parseJSON(this.jsonStr);
+	var envLength = jsonStrObj.length;
+	if (envLength != 'undefined' && envLength > 1) {
+	
+		for (i = 0; i < envLength; i++) {
+			var environment = jsonStrObj[i];
+			var env = environment["@name"];
+			var envDefault = environment["@default"];
+			if (this.currentEnv != undefined) {
+				if (this.currentEnv == env) {
+					json = getConfigJson(environment, this.type, this.name);
+				}
+			} else if (envDefault == "true") {
+				json = getConfigJson(environment, this.type, this.name);
+			}
+
+		}
+	} else {
+		var environment = jsonStrObj[0];
+		var env = environment["@name"];
+        var envDefault = environment["@default"];
+		json = getConfigJson(environment, this.type, this.name);
+	}
+	return json;
+}
+
+function getConfigJson(environment, type, name) {
+	var json = {};
+    var configurations = environment[type];
+	if (configurations.length > 1) {
+        for (var i = 0; i < configurations.length; i++) {
+            if (name != undefined) {
+                var configName = configurations[i]["@name"];
+				if (configName == name) {
+                    json = configurations[i];
+                }
+            } else {
+                json = configurations[0];
+            }
+        }
+    } else {
+        json = configurations;
+    }
+    return json;
+}
