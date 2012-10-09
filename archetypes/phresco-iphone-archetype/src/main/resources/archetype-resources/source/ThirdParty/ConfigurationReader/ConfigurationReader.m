846e51d7766af415c5578200d967dd1431301592
mahesh <mahesh.prathapaneni@photoninfotech.net>
2012-10-09 20:04:52 +0530

    [artf542497] archetype changes for blackberry

--- archetypes/phresco-iphone-archetype/src/main/resources/archetype-resources/source/ThirdParty/ConfigurationReader/ConfigurationReader.m
+++ archetypes/phresco-iphone-archetype/src/main/resources/archetype-resources/source/ThirdParty/ConfigurationReader/ConfigurationReader.m
@@ -7,7 +7,6 @@
 //
 
 #import "ConfigurationReader.h"
-#import "Base64.h"
 
 @implementation ConfigurationReader
 @synthesize stories;
@@ -21,22 +20,8 @@
 	
     // here, for some reason you have to use NSClassFromString when trying to alloc NSXMLParser, otherwise you will get an object not found error
     // this may be necessary only for the toolchain
-    /* NSStringEncoding encoding = NSASCIIStringEncoding;
-    NSError* error;
-    
-	NSString *str = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"phresco-env-config" ofType:@"xml"] usedEncoding:&encoding error:&error];
-    
-    NSString *trimmedString = [str stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-    
-    [Base64 initialize];
-    
-    NSData	*b64DecData = [Base64 decode:trimmedString];
-    
-    NSString* decryptedStr = [[NSString alloc] initWithData:b64DecData encoding:NSASCIIStringEncoding];
-    
-	NSLog(@"decrypted string = %@",decryptedStr);*/
     NSString *xmlPath = [[NSBundle mainBundle] pathForResource:@"phresco-env-config" ofType:@"xml"];
-     NSData *xmlData = [NSData dataWithContentsOfFile:xmlPath];
+    NSData *xmlData = [NSData dataWithContentsOfFile:xmlPath];
     rssParser = [[NSXMLParser alloc] initWithData:xmlData];
 	
     // Set self as the delegate of the parser so that it will receive the parser delegate methods callbacks.
@@ -57,17 +42,18 @@
 	
 	UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"Error loading content" message:errorString delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
 	[errorAlert show];
+    [errorAlert release];
 }
 
 - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict{			
     //NSLog(@"found this element: %@", elementName);
 	currentElement = [elementName copy];
-    if(@"environment") {
+    
 		if([elementName isEqualToString:@"environment"]) {
 			//NSString *relAtt = [attributeDict valueForKey:@"url"];
 			thisURL  = [attributeDict valueForKey:@"default"];
             NSLog(@"environment: %@", thisURL);
-        }
+        
     }
 	if ([elementName isEqualToString:@"WebService"]) {
 		// clear out our story item caches...
@@ -86,10 +72,10 @@
 
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{     
 	//NSLog(@"ended element: %@", elementName);
-   
-   /* envVar = @"mywebservice";
-    if (([envVar isEqualToString:thisLength])&& ([thisURL isEqualToString:@"true"])){*/
-         if(@"WebService") {
+    
+    
+     if ([thisURL isEqualToString:@"true"]){
+    if(@"WebService") {
         if ([elementName isEqualToString:@"WebService"]){
             // save values to an item, then store that item into the array...
             
@@ -97,33 +83,30 @@
             [item setObject:currentLink forKey:@"port"];
             [item setObject:currentSummary forKey:@"protocol"];
             [item setObject:currentDate forKey:@"context"];
-            [item setObject:thisLength forKey:@"name"];
             NSLog(@"currentDate: %@", currentDate);
             [stories addObject:[item copy]];
             NSLog(@"adding story: %@", currentTitle);
         }
-        // }
+         }
     }
-     
+    
 }
 
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
 	//NSLog(@"found characters: %@", string);
 	// save the characters for the current item...
     if([currentNode isEqualToString:@"WebService"] ) {
-	if ([currentElement isEqualToString:@"host"]) {
-		[currentTitle appendString:string];
-	} else if ([currentElement isEqualToString:@"port"]) {
-		[currentLink appendString:string];
-	} else if ([currentElement isEqualToString:@"protocol"]) {
-		[currentSummary appendString:string];
-	} else if ([currentElement isEqualToString:@"context"]) {
-		[currentDate appendString:string];
-    } else if ([currentElement isEqualToString:@"name"]) {
-		[thisLength appendString:string];
-    }
+        if ([currentElement isEqualToString:@"host"]) {
+            [currentTitle appendString:string];
+        } else if ([currentElement isEqualToString:@"port"]) {
+            [currentLink appendString:string];
+        } else if ([currentElement isEqualToString:@"protocol"]) {
+            [currentSummary appendString:string];
+        } else if ([currentElement isEqualToString:@"context"]) {
+            [currentDate appendString:string];
+        }
         
-     }
+    }
 	
 }
 
