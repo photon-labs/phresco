3a61bec1459123a8aff1147051b4a6264d90e83c
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2012-10-12 11:01:03 +0530

    checker file changed

--- framework/phresco-framework-runner/delivery/bin/env.sh
+++ framework/phresco-framework-runner/delivery/bin/env.sh
@@ -1,7 +1,7 @@
 export PHRESCO_HOME=$PWD/..
 export MAVEN_HOME=$PHRESCO_HOME/tools/maven
 export M2_HOME=$MAVEN_HOME
-export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker-267
+export CHECKER_HOME=$PHRESCO_HOME/workspace/tools/checker-269
 export CHIEF_HOME=$PHRESCO_HOME/workspace/tools/chief
 export WAXSIM_HOME=$PHRESCO_HOME/workspace/tools/waxsim/usr/local/bin/waxsim
 export PATH=$WAXSIM_HOME:$CHIEF_HOME:$CHECKER_HOME:$MAVEN_HOME/bin:$PHRESCO_HOME/bin:$PHRESCO_HOME/workspace/tools/phantomjs-1.5.0/bin:$PATH
