260412e00a4a8e3bcd3457ae6c3d1429371f8dfd
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2012-10-12 11:59:39 +0530

    iphone reverting changes

---
+++ archetypes/phresco-iphone-archetype/src/main/resources/archetype-resources/source/AllTest/AllTest.h
@@ -0,0 +1,32 @@
+/*
+ * ###
+ * PHR_iphone-native-hw
+ * %%
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
+//
+//  AllTest.h
+//  AllTest
+//
+//  Created by Administrator on 20/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AllTest : SenTestCase
+
+@end
---
+++ archetypes/phresco-iphone-archetype/src/main/resources/archetype-resources/source/ThirdParty/ConfigurationReader/ConfigurationReader.m
@@ -0,0 +1,140 @@
+//
+//  ConfigurationReader.m
+//  Phresco
+//
+//  Created by Administrator on 02/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ConfigurationReader.h"
+#import "Base64.h"
+
+@implementation ConfigurationReader
+@synthesize stories;
+
+- (void)parseXMLFileAtURL:(NSString *)config environment:(NSString *)name
+{	
+	stories = [[NSMutableArray alloc] init];
+	
+    //you must then convert the path to a proper NSURL or it won't work
+    //NSURL *xmlURL = [NSURL URLWithString:URL];
+	
+    // here, for some reason you have to use NSClassFromString when trying to alloc NSXMLParser, otherwise you will get an object not found error
+    // this may be necessary only for the toolchain
+    /* NSStringEncoding encoding = NSASCIIStringEncoding;
+    NSError* error;
+    
+	NSString *str = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"phresco-env-config" ofType:@"xml"] usedEncoding:&encoding error:&error];
+    
+    NSString *trimmedString = [str stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    
+    [Base64 initialize];
+    
+    NSData	*b64DecData = [Base64 decode:trimmedString];
+    
+    NSString* decryptedStr = [[NSString alloc] initWithData:b64DecData encoding:NSASCIIStringEncoding];
+    
+	NSLog(@"decrypted string = %@",decryptedStr);*/
+    NSString *xmlPath = [[NSBundle mainBundle] pathForResource:@"phresco-env-config" ofType:@"xml"];
+     NSData *xmlData = [NSData dataWithContentsOfFile:xmlPath];
+    rssParser = [[NSXMLParser alloc] initWithData:xmlData];
+	
+    // Set self as the delegate of the parser so that it will receive the parser delegate methods callbacks.
+    [rssParser setDelegate:self];
+	
+    // Depending on the XML document you're parsing, you may want to enable these features of NSXMLParser.
+    [rssParser setShouldProcessNamespaces:NO];
+    [rssParser setShouldReportNamespacePrefixes:NO];
+    [rssParser setShouldResolveExternalEntities:NO];
+	
+    [rssParser parse];
+	
+}
+
+- (void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError {
+	NSString * errorString = [NSString stringWithFormat:@"Unable to download story feed from web site (Error code %i )", [parseError code]];
+	NSLog(@"error parsing XML: %@", errorString);
+	
+	UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"Error loading content" message:errorString delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	[errorAlert show];
+}
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict{			
+    //NSLog(@"found this element: %@", elementName);
+	currentElement = [elementName copy];
+    if(@"environment") {
+		if([elementName isEqualToString:@"environment"]) {
+			//NSString *relAtt = [attributeDict valueForKey:@"url"];
+			thisURL  = [attributeDict valueForKey:@"default"];
+            NSLog(@"environment: %@", thisURL);
+        }
+    }
+	if ([elementName isEqualToString:@"WebService"]) {
+		// clear out our story item caches...
+        currentNode = @"WebService";
+		item = [[NSMutableDictionary alloc] init];
+		currentTitle = [[NSMutableString alloc] init];
+		currentDate = [[NSMutableString alloc] init];
+		currentSummary = [[NSMutableString alloc] init];
+		currentLink = [[NSMutableString alloc] init];
+        
+        thisLength  = [attributeDict valueForKey:@"name"];
+        NSLog(@"server: %@", thisLength);
+	}
+	
+}
+
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{     
+	//NSLog(@"ended element: %@", elementName);
+   
+   /* envVar = @"mywebservice";
+    if (([envVar isEqualToString:thisLength])&& ([thisURL isEqualToString:@"true"])){*/
+         if(@"WebService") {
+        if ([elementName isEqualToString:@"WebService"]){
+            // save values to an item, then store that item into the array...
+            
+            [item setObject:currentTitle forKey:@"host"];
+            [item setObject:currentLink forKey:@"port"];
+            [item setObject:currentSummary forKey:@"protocol"];
+            [item setObject:currentDate forKey:@"context"];
+            [item setObject:thisLength forKey:@"name"];
+            NSLog(@"currentDate: %@", currentDate);
+            [stories addObject:[item copy]];
+            NSLog(@"adding story: %@", currentTitle);
+        }
+        // }
+    }
+     
+}
+
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
+	//NSLog(@"found characters: %@", string);
+	// save the characters for the current item...
+    if([currentNode isEqualToString:@"WebService"] ) {
+	if ([currentElement isEqualToString:@"host"]) {
+		[currentTitle appendString:string];
+	} else if ([currentElement isEqualToString:@"port"]) {
+		[currentLink appendString:string];
+	} else if ([currentElement isEqualToString:@"protocol"]) {
+		[currentSummary appendString:string];
+	} else if ([currentElement isEqualToString:@"context"]) {
+		[currentDate appendString:string];
+    } else if ([currentElement isEqualToString:@"name"]) {
+		[thisLength appendString:string];
+    }
+        
+     }
+	
+}
+
+- (void)parserDidEndDocument:(NSXMLParser *)parser {
+	
+	[activityIndicator stopAnimating];
+	[activityIndicator removeFromSuperview];
+	
+	NSLog(@"all done!");
+	NSLog(@"stories array has %d items", [stories count]);
+	
+}
+
+@end
