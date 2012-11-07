e332819224efe535a7d3f611956c44951d26b95f
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2012-11-07 11:53:56 +0530

    Archetype commit for Corodova support for iphone

---
+++ archetypes/phresco-iphone-hybrid-archetype/src/main/resources/archetype-resources/source/HelloWorld/Classes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld_Cordova
+//
+//  Created by Sathiyasuresh on 03/10/12.
+//  Copyright (c) 2012 Sathiyasuresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
