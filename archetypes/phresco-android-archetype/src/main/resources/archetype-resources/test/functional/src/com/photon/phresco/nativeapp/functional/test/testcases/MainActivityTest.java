7cd3796c3baa77a826aedc2cf752d292de313df4
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2012-11-07 12:27:41 +0530

    functinal test case changes made in android native

--- archetypes/phresco-android-archetype/src/main/resources/archetype-resources/test/functional/src/com/photon/phresco/nativeapp/functional/test/testcases/MainActivityTest.java
+++ archetypes/phresco-android-archetype/src/main/resources/archetype-resources/test/functional/src/com/photon/phresco/nativeapp/functional/test/testcases/MainActivityTest.java
@@ -1,9 +1,9 @@
 /*
  * ###
- * Archetype - phresco-android-native-archetype
- * 
+ * PHR_AndroidNative
+ * %%
  * Copyright (C) 1999 - 2012 Photon Infotech Inc.
- * 
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,63 +17,111 @@
  * limitations under the License.
  * ###
  */
+/**
+ * Author by {phresco} QA Automation Team
+ */
 package com.photon.phresco.nativeapp.functional.test.testcases;
 
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.TextView;
+import android.util.Log;
 
 import com.jayway.android.robotium.solo.Solo;
-import com.photon.phresco.nativeapp.R;
 import com.photon.phresco.nativeapp.activity.MainActivity;
+import com.photon.phresco.nativeapp.functional.test.core.Data;
+
 
 @SuppressWarnings("unchecked")
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-	private static Class<MainActivity> mainActivity;
-	private Solo soloObj;
-	private TextView mTextView;
-	private String resourceString;
-	private static final String TARGET_PACKAGE = "com.photon.phresco.nativeapp";
-	private static final String LAUNCHER_ACTIVITY = "com.photon.phresco.nativeapp.activity.MainActivity";
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
 
+	/**
+	 * This is suite testcase by this testcase will call other testcases . In
+	 * static block we are loading the MainActivity class and from the
+	 * constructor will pass the package and activity full class name then in
+	 * setUp() created the Solo class object
+	 * 
+	 */
+	public static final String PACKAGE_NAME = "com.photon.phresco.nativeapp";
+	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.photon.phresco.nativeapp.activity.MainActivity";
+	private static Class<MainActivity> mainActivity;
+	private Solo soloMain;
+
+	private static final String TAG = "****MainTestCase****";
+	private Instrumentation inst;
+	private Data data;
+
+	/**
+	 * This block will be executed first and it will loads the SplashActivity .
+	 */
 	static {
 		try {
+			mainActivity = (Class<MainActivity>) Class
+					.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
+		}
 
-			mainActivity = (Class<MainActivity>) Class.forName(LAUNCHER_ACTIVITY);
-		} catch (ClassNotFoundException e) {
+		catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public MainActivityTest() throws Exception {
-		super(TARGET_PACKAGE, mainActivity);
+	/**
+	 * In this constructor , we have to send the packagename and activity full
+	 * class name.
+	 * 
+	 * @throws Exception
+	 */
+	public MainActivityTest() throws TestException {
+		super(PACKAGE_NAME, mainActivity);
 	}
 
+	/**
+	 * this method for create the Solo class object having two super class
+	 * methods..
+	 * 
+	 */
 	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		soloObj = new Solo(getInstrumentation(), getActivity());
-		mTextView = (TextView) soloObj.getView(R.id.textview);
-		resourceString = soloObj.getString(R.string.hello);
+	public void setUp() {
+
+		soloMain = new Solo(getInstrumentation(), getActivity());
+		inst=getInstrumentation();
+		data=new Data();
 
 	}
 
+	/**
+	 * With this method we can check the Registration Verification scenario by
+	 * using testRegisterScenario().
+	 * 
+	 */
+
+	public void testMain() throws TestException {
+		
+		data.parser(inst.getContext());
+		soloMain.clickOnButton(0);
+		soloMain.sleep(2000);
+		
+		assertEquals("Hello World!",data.HELLO_WORLD_TEXT);
+		soloMain.sleep(2000);
+
+	}
+
+
+	/**
+	 * Once the testcases executed completely. This method will be called and
+	 * will close the all activities this is overridden with super class
+	 * tearDown()method
+	 */
 	@Override
-	public void tearDown() throws Exception {
+	protected void tearDown() throws Exception {
+
 		try {
-			soloObj.finalize();
-
-		} catch (Throwable e) {
-			e.printStackTrace();
+			soloMain.finishOpenedActivities();
+		} catch (Exception e) {
+			Log.e(TAG, Log.getStackTraceString(e));
 		}
 		getActivity().finish();
 		super.tearDown();
-	}
-
-	public void testMain() throws InterruptedException {
-		assertTrue(soloObj != null);
-		assertNotNull(mTextView);
-		assertNotNull(resourceString);
-		assertEquals(mTextView.getText().toString(), resourceString);
 
 	}
 
